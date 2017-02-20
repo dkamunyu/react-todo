d71c3d7395cb0e64af37fa6e55df7276dc3330b6
dkamunyu <dkamunyu@github.com>
2017-02-20 03:26:48 +0300

    Added ToDoApp component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+let React = require ('react');
+
+let TodoApp = React.createClass({
+    
+    render: function(){
+        return(
+        <div>
+            TodoApp
+        </div>
+        )
+    }
+});
+
+module.exports = TodoApp;
